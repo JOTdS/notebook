92f66d34616bc6c02a876ab55e143a58ce0200bb
José Otávio <j.otavio.ts@gmail.com>
2022-05-28 10:01:41 -0300

    Ajustando base inicial

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -4,6 +4,9 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+
 class CreateUsersTable extends Migration
 {
     /**
@@ -18,10 +21,19 @@ class CreateUsersTable extends Migration
             $table->string('name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
+            $table->string('tipo',3);
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
         });
+
+        //Adicionando usuário Administrador para o sistema
+        $user = new User();
+        $user->name = "Administrador";
+        $user->email = "admin@admin.com";
+        $user->tipo = 'adm';
+        $user->password = Hash::make("admin123");
+        $user->save();
     }
 
     /**
